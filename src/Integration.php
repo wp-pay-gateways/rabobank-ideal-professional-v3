1f2944ba8108f472884fa0565357c25cba3e9d37
Reüel van der Steege <reuel@pronamic.nl>
2016-03-03 16:39:17 +0100

    Add product and dashboard URLs

--- src/Integration.php
+++ src/Integration.php
@@ -11,9 +11,14 @@
  */
 class Pronamic_WP_Pay_Gateways_Rabobank_IDealAdvancedV3_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_AbstractIntegration {
 	public function __construct() {
-		$this->id       = 'rabobank-ideal-professional-v3';
-		$this->name     = 'Rabobank - iDEAL Professional - v3';
-		$this->provider = 'rabobank';
+		$this->id            = 'rabobank-ideal-professional-v3';
+		$this->name          = 'Rabobank - iDEAL Professional (v3)';
+		$this->provider      = 'rabobank';
+		$this->product_url   = 'https://www.rabobank.nl/bedrijven/betalen/geld-ontvangen/ideal-professional/';
+		$this->dashboard_url = array(
+			'test' => 'https://idealtest.rabobank.nl/',
+			'live' => 'https://ideal.rabobank.nl/',
+		);
 	}
 
 	public function get_config_factory_class() {
