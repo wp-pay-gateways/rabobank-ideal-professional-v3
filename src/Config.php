c38aa0d6bfd9695734f8848590206a0fd9809362
Remco Tolsma <info@remcotolsma.nl>
2017-05-01 14:53:57 +0200

    Getting ready for version 1.0.2.

--- src/Config.php
+++ src/Config.php
@@ -3,11 +3,12 @@
 /**
  * Title: Rabobank - iDEAL Professional - v3 config
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2017
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.2
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_Rabobank_IDealAdvancedV3_Config extends Pronamic_WP_Pay_Gateways_IDealAdvancedV3_Config {
 	public function get_payment_server_url() {
