9d30b0a90a9e35d73061eda3f2fa30db0a9fc56a
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 12:07:56 +0100

    Happy 2018.

--- tests/TestConfigTest.php
+++ tests/TestConfigTest.php
@@ -3,7 +3,7 @@
 /**
  * Title: Rabobank - iDEAL Professional - v3 - Test config test
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
